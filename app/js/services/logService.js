9c2dacd91e328e1347fb5d6bf985af1f7b681839
Bruno Menezes <brunodmenezes@gmail.com>
2018-10-15 21:33:49 +1300

    added way to clean the logs. refactored way how to include DOM elements and remove from debug container.

--- app/js/services/logService.js
+++ app/js/services/logService.js
@@ -1,20 +1,38 @@
 const debugContainer = document.getElementById( "debugContainer" );
+const clearBtn = document.getElementById( "clearLogs" );
+
+clearBtn.onclick = clear;
 
 function log( msg ) {
-    render(`<p>${msg}</p>`);
+    render(createParentheses(msg));
 }
 
 function info(msg) {
-    render(`<p class="info">${msg}</p>`);
+    render(createParentheses(msg, 'info'));
 }
 
 function error(msg) {
-    render(`<p class="error">${msg}</p>`);
+    render(createParentheses(msg, 'error'));
 }
 
-function render(html) {
-    if(!html) return;
-    debugContainer.innerHTML += html;
+function render(node) {
+    if(!node) return;
+    debugContainer.insertBefore(node, debugContainer.firstChild);
+}
+
+function createParentheses(message, clazz) {
+    const date = new Date();
+    const node = document.createElement('p');
+    const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    node.textContent = `${date.getHours()}:${date.getMinutes()}:${seconds} - ${message}`;
+    node.className = clazz;
+    return node;
+}
+
+function clear() {
+    while(debugContainer.firstChild) {
+        debugContainer.removeChild(debugContainer.firstChild);
+    }
 }
 
 module.exports = {log, info, error};
