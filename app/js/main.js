1a63241c79f5b7b625d70b2136287654290b5a86
Bruno Menezes <brunodmenezes@gmail.com>
2018-10-15 21:00:35 +1300

    adding some styles, refactored main to have minimum needed to bootstrap the app.

--- app/js/main.js
+++ app/js/main.js
@@ -3,36 +3,38 @@ const logService = require('./services/logService');
 const canvasService = require('./services/canvasService');
 const mathService = require('./services/mathService');
 const interactionService = require('./services/interactionService');
+const printService = require('./services/printService');
 
 const fileSelector = document.getElementById("fileSelector");
-const generateButton = document.getElementById("generateButton");
 
-canvasService
-    .initialize()
-    .then(interactionService.initialize);
+function initialize() {
+    canvasService
+        .initialize()
+        .then(interactionService.initialize);
 
-fileSelector.onchange = (e) => {
-    imageService
-        .loadImage(e)
-        .then(
-            ({img, fileName}) => {
-                let imageData = {
-                    id: fileName,
-                    width: mathService.pixelsToInches(img.naturalWidth),
-                    height: mathService.pixelsToInches(img.naturalHeight),
-                    img,
-                    x:0,
-                    y:0
-                };
-                canvasService.render(imageData);
-                logService.info("Loaded Image w/dimensions " + imageData.width + " x " + imageData.height);
-            },
-            err => logService.error(err.message || err)
-        );
-};
+    printService.initialize();
 
-generateButton.onclick = function (e) {
-    log("GENERATE BUTTON CLICKED!! Should this do something else?");
-};
+    fileSelector.onchange = (e) => {
+        imageService
+            .loadImage(e)
+            .then(
+                ({img, fileName}) => {
+                    let imageData = {
+                        id: fileName,
+                        width: mathService.pixelsToInches(img.naturalWidth),
+                        height: mathService.pixelsToInches(img.naturalHeight),
+                        img,
+                        x:0,
+                        y:0
+                    };
+                    canvasService.render(imageData);
+                    logService.info("Loaded Image w/dimensions " + imageData.width + " x " + imageData.height);
+                },
+                err => logService.error(err.message || err)
+            );
+    };
+}
+
+initialize();
 
 logService.log("Test application ready");
